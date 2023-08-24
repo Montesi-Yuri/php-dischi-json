0150959cd240440b68d63ac877edc60fd2bbab87
Montesi-Yuri <yuri13312@gmail.com>
2023-08-24 17:08:21 +0200

    ADD - content in index with Vue

--- api.php
+++ api.php
@@ -2,8 +2,3 @@
     $discsString = file_get_contents('database/discs.json');
 
     $discsArr = json_decode($discsString, true);
-    
-    
-
-
-    
--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        message: 'Hello Vue!'
+            discs : []
         }
     },
     created(){
-        axios.get('')
-    }
+        axios.get('http://localhost/php-dischi-json/database/discs.json')
+        .then(response => {
+            this.discs = response.data;
+            console.log(this.discs);
+        })
+    },
 }).mount('#app')
